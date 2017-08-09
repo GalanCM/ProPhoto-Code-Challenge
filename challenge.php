56b25611ead2aecae036cabc37bc680b3ab26efc
Galan Montgomery <galan.c.m@gmail.com>
2017-08-09 14:33:11 -0400

    Clean up Wordpress code

--- challenge.php
+++ challenge.php
@@ -6,12 +6,12 @@ Plugin Name: Challenge Plugin
 function challenge_page()
 {
     add_menu_page(
-        'Challenge',
-        'Challenge',
+        "Galan's Plugin",
+        "Galan's Plugin",
         'manage_options',
         plugin_dir_path(__FILE__) . 'view.php',
         null,
-        '',
+        'dashicons-thumbs-up',
         100
     );
 }
@@ -20,14 +20,11 @@ add_action('admin_menu', 'challenge_page');
 
 function make_nonce() {
   $nonce = wp_create_nonce('wp_rest');
-  // echo $nonce;
-  // echo wp_nonce_url( "index.php/wp-json/wp/v2/posts/", 'edit_title' );
   wp_localize_script( 'bundle', 'nonce', $nonce );
 }
 
 
 if (isset($_GET['page']) && ($_GET['page'] == 'challenge/view.php')) {
-  // wp_register_script( 'build', 'path/to/myscript.js' );
   wp_enqueue_script( 'bundle', plugins_url('', __FILE__ ) . '/build/bundle.js', [], 1.0, true);
   add_action('admin_menu', 'make_nonce');
 }
