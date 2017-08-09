a3435183309a583da89eb0d73c89bda6a7aac598
Galan Montgomery <galan.c.m@gmail.com>
2017-08-09 12:18:39 -0400

    Confirm post deletion

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -51,17 +51,18 @@ export default class App extends React.Component {
   }
 
   delete(e) {
-    var httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = () => {
-      if (httpRequest.readyState === XMLHttpRequest.DONE) {
-        let json = JSON.parse( httpRequest.responseText );
-        this.props.delete( this.props.data.id );
-      } else {
-        // Not ready yet.
-      }
-    };
-    httpRequest.open('DELETE', '../index.php/wp-json/wp/v2/posts/' + this.props.data.id );
-    httpRequest.setRequestHeader( 'X-WP-Nonce', nonce );
-    httpRequest.send();
+    if ( confirm("Are you sure you want to delete this post?") ) {
+      var httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = () => {
+        if (httpRequest.readyState === XMLHttpRequest.DONE) {
+          this.props.delete( this.props.data.id );
+        } else {
+          // Not ready yet.
+        }
+      };
+      httpRequest.open('DELETE', '../index.php/wp-json/wp/v2/posts/' + this.props.data.id );
+      httpRequest.setRequestHeader( 'X-WP-Nonce', nonce );
+      httpRequest.send();
+    }
   }
 }
