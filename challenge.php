d777f630204376af71174465f2ffb1bd05d5faa1
Galan Montgomery <galan.c.m@gmail.com>
2017-08-09 11:39:29 -0400

    Committed uncommited files??

--- challenge.php
+++ challenge.php
@@ -18,7 +18,17 @@ function challenge_page()
 add_action('admin_menu', 'challenge_page');
 
 
+function make_nonce() {
+  $nonce = wp_create_nonce('wp_rest');
+  // echo $nonce;
+  // echo wp_nonce_url( "index.php/wp-json/wp/v2/posts/", 'edit_title' );
+  wp_localize_script( 'bundle', 'nonce', $nonce );
+}
+
+
 if (isset($_GET['page']) && ($_GET['page'] == 'challenge/view.php')) {
+  // wp_register_script( 'build', 'path/to/myscript.js' );
   wp_enqueue_script( 'bundle', plugins_url('', __FILE__ ) . '/build/bundle.js', [], 1.0, true);
+  add_action('admin_menu', 'make_nonce');
 }
 ?>
